425e083ed20e5618d4773f957f3bbbaca9d81b61
Aayush Shrestha <aayushshres20@gmail.com>
2024-05-14 08:11:31 +0530

    image src fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Terminal from "./components/terminal";
 function App() {
   return (
     <div className="App">
-      <img src="src/assets/scanlines.png" className="noselect" id="scan"></img>
+      <img src="/src/assets/scanlines.png" className="noselect" id="scan"></img>
       <Terminal></Terminal>
     </div>
   );
